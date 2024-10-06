47e78201b3251c10c0d4b6416e2363b175f78d09
SYED HASNAI ATTARI <syedhasnainattari5@gmail.com>
2024-10-06 20:14:29 +0500

    Add files via upload

---
+++ DICE PROJECT/app.js
@@ -0,0 +1,71 @@
+
+.container {
+    width: 70%;
+    margin: auto;
+    text-align: center;
+}
+
+.dice {
+    text-align: center;
+    display: inline-block;
+    margin: 10px;
+}
+
+body {
+    background-image: url(./img/final.jpg);
+    opacity: 50 !important;
+    margin: 0;
+    background-repeat: no-repeat;
+    width: 100% !important;
+    height: 100%;
+}
+
+h1 {
+    margin: 30px;
+    font-family: "Lobster", cursive;
+    text-shadow: 5px 0 #232931;
+    font-size: 4.5rem;
+    color: #000000;
+    text-align: center;
+}
+
+p {
+    font-size: 2rem;
+    color: #000000;
+    font-family: "Indie Flower", cursive;
+}
+
+img {
+    width: 100%;
+}
+
+.bottom {
+    padding-top: 30px;
+}
+
+.btn {
+    background: #ffe100;
+    font-family: "Indie Flower", cursive;
+    border-radius: 7px;
+    color: #ffff;
+    font-size: 30px;
+    padding: 16px 25px 16px 25px;
+    text-decoration: none;
+}
+
+.butn:hover {
+    background: #b4cf05;
+    text-decoration: none;
+}
+.wow{
+  color: #ffe100;
+  font-size: medium;
+}
+
+
+.even-roll {
+transition: transform 1.5s ease-out;
+}
+.odd-roll {
+transition: transform 1.25s ease-out;
+}
